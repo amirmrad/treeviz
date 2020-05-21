21fcf41bf07c5c13cbc88b47f4609b56019a27f6
Amir Hossein Rad <ahmr10@gmail.com>
2020-05-21 22:34:22 +0100

    test.ts

--- static/index.js
+++ static/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+function setup() {
+  createCanvas(900,600)
+  background(128);
+  ellipse(width/2, height/2, 20, 20)
+  
+
+
+}
+
+let startX = 0
+let startY = 0
+function draw() {
+}
